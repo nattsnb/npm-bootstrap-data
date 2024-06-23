9798992b3437fa841052338c74d4ebace65fc4ae
Natasza ASzatan <natiye.nb@gmai.com>
2024-06-23 12:10:28 +0100

    created two first exercises

--- app/index.js
+++ app/index.js
@@ -1,6 +1,3 @@
 import './styles/styles.scss';
 
-window.onload = () => {
-  const heading = document.querySelector('.heading');
-  heading.textContent = 'It\'s working!';
-};
+console.log("it's working")
