7a555435b26f3a332f2561634ab2a8f9ec733ca3
Natasza ASzatan <natiye.nb@gmai.com>
2024-07-03 18:18:01 +0100

    build data structure needed for further application

--- app/DataStructure.js
+++ app/DataStructure.js
@@ -18,23 +18,23 @@ export class DataStructure {
     this.finalData = [];
     for (let i = 0; i < this.companiesData.length; i++) {
       this.finalData[i] = { name: this.companiesData[i].name };
-      const lookFor = this.companiesData[i].uri;
+      const companyUri = this.companiesData[i].uri;
+      let users = []
       for (let j = 0; j < this.usersData.length; j++) {
-        if (this.usersData[j].uris.company === lookFor) {
-          const newUser = {
-            user: {
+        if (this.usersData[j].uris.company === companyUri) {
+          const user = {
               name: this.usersData[j].name,
               email: this.usersData[j].email,
-            },
           };
-          this.finalData[i].users = { ...newUser };
+          users.push(user)
         }
       }
+      this.finalData[i].users = users
     }
-    console.log(this.finalData);
   };
 
   findItemById(list, id) {
     return list.find((obj) => obj.id === id).name;
   }
 }
+
