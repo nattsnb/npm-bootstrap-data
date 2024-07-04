a25949ffb1df928ca8ae8cf740ed01ca34a0260d
Natasza ASzatan <natiye.nb@gmai.com>
2024-07-04 14:52:49 +0100

    ran prettier

--- app/DataStructure.js
+++ app/DataStructure.js
@@ -19,22 +19,23 @@ export class DataStructure {
     for (let i = 0; i < this.companiesData.length; i++) {
       this.finalData[i] = { name: this.companiesData[i].name };
       const companyUri = this.companiesData[i].uri;
-      let users = []
+      let users = [];
       for (let j = 0; j < this.usersData.length; j++) {
         if (this.usersData[j].uris.company === companyUri) {
           const user = {
-              name: this.usersData[j].name,
-              email: this.usersData[j].email,
+            name: this.usersData[j].name,
+            email: this.usersData[j].email,
           };
-          users.push(user)
+          users.push(user);
         }
       }
-      this.finalData[i].users = users
+      this.finalData[i].users = users;
+      this.finalData[i].numberOfUsers = this.finalData[i].users.length;
     }
+    console.log(this.finalData);
   };
 
   findItemById(list, id) {
     return list.find((obj) => obj.id === id).name;
   }
 }
-
