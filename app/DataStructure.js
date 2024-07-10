fcba988558f71e660bd99d4e124281a3a6b25b0d
Natasza ASzatan <natiye.nb@gmai.com>
2024-07-10 10:08:46 +0100

    fixed complexity of produceData function using filter

--- app/DataStructure.js
+++ app/DataStructure.js
@@ -21,20 +21,13 @@ export class DataStructure {
     for (let i = 0; i < this.companiesData.length; i++) {
       this.finalData[i] = { name: this.companiesData[i].name };
       const companyUri = this.companiesData[i].uri;
-      let users = [];
-      for (let j = 0; j < this.usersData.length; j++) {
-        if (this.usersData[j].uris.company === companyUri) {
-          const user = {
-            name: this.usersData[j].name,
-            email: this.usersData[j].email,
-          };
-          users.push(user);
+      this.finalData[i].users = this.usersData.filter(
+        function checkIfBelongsToCompany(user) {
+          return user.uris.company === companyUri;
         }
-      }
-      this.finalData[i].users = users;
+      );
       this.finalData[i].numberOfUsers = this.finalData[i].users.length;
     }
-    console.log(this.finalData);
     this.populateTable();
   };
 
