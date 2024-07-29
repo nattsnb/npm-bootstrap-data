29df6bd247df1c7af69dbbbd50868fb25692a5a8
Natasza ASzatan <natiye.nb@gmai.com>
2024-07-29 08:38:47 +0100

    changed name of variable

--- app/DataStructure.js
+++ app/DataStructure.js
@@ -17,29 +17,29 @@ export class DataStructure {
   };
 
   produceData = () => {
-    this.companiesNumberOfUsers = [];
+    this.companiesUsersNumberAndInfo = [];
     for (let i = 0; i < this.companiesData.length; i++) {
-      this.companiesNumberOfUsers[i] = { name: this.companiesData[i].name };
+      this.companiesUsersNumberAndInfo[i] = { name: this.companiesData[i].name };
       const companyUri = this.companiesData[i].uri;
-      this.companiesNumberOfUsers[i].users = this.usersData.filter(
+      this.companiesUsersNumberAndInfo[i].users = this.usersData.filter(
         function checkIfBelongsToCompany(user) {
           return user.uris.company === companyUri;
         }
       );
-      this.companiesNumberOfUsers[i].numberOfUsers = this.companiesNumberOfUsers[i].users.length;
+      this.companiesUsersNumberAndInfo[i].numberOfUsers = this.companiesUsersNumberAndInfo[i].users.length;
     }
-    this.companiesNumberOfUsers.sort((a, b) => a.numberOfUsers - b.numberOfUsers)
+    this.companiesUsersNumberAndInfo.sort((a, b) => a.numberOfUsers - b.numberOfUsers)
     this.populateTable();
   };
 
   populateTable = () => {
     const tableBody = document.querySelector(".table-body");
-    for (let i = 0; i < this.companiesNumberOfUsers.length; i++) {
+    for (let i = 0; i < this.companiesUsersNumberAndInfo.length; i++) {
       const row = document.createElement("tr");
       this.createCompanyEntry(tableBody, row, i);
       const nestedTable = document.createElement("table");
       nestedTable.classList.add("nested-table");
-      for (let j = 0; j < this.companiesNumberOfUsers[i].numberOfUsers; j++) {
+      for (let j = 0; j < this.companiesUsersNumberAndInfo[i].numberOfUsers; j++) {
         this.createNestedEntry(nestedTable, i, j);
       }
       const showButton = new ShowButton(nestedTable, row);
@@ -51,8 +51,8 @@ export class DataStructure {
     const cell2 = document.createElement("td");
     const cell3 = document.createElement("td");
     cell1.innerHTML = i + 1;
-    cell2.innerHTML = this.companiesNumberOfUsers[i].name;
-    cell3.innerHTML = this.companiesNumberOfUsers[i].numberOfUsers;
+    cell2.innerHTML = this.companiesUsersNumberAndInfo[i].name;
+    cell3.innerHTML = this.companiesUsersNumberAndInfo[i].numberOfUsers;
     row.appendChild(cell1);
     row.appendChild(cell2);
     row.appendChild(cell3);
@@ -62,8 +62,8 @@ export class DataStructure {
     const nestedRow = document.createElement("tr");
     const nestedCell1 = document.createElement("td");
     const nestedCell2 = document.createElement("td");
-    nestedCell1.innerHTML = this.companiesNumberOfUsers[i].users[j].name;
-    nestedCell2.innerHTML = this.companiesNumberOfUsers[i].users[j].email;
+    nestedCell1.innerHTML = this.companiesUsersNumberAndInfo[i].users[j].name;
+    nestedCell2.innerHTML = this.companiesUsersNumberAndInfo[i].users[j].email;
     nestedRow.appendChild(nestedCell1);
     nestedRow.appendChild(nestedCell2);
     nestedTable.appendChild(nestedRow);
